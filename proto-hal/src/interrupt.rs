e6cd608a52a352e954d303121fa9aa59d48c269a
Adin Ackerman <adinackerman@gmail.com>
2025-12-06 12:47:33 -0800

    Update interrupt.rs

--- proto-hal/src/interrupt.rs
+++ proto-hal/src/interrupt.rs
@@ -1,24 +1,23 @@
-/// Represents a vector table entry, i.e.
-/// a function pointer.
+/// Represents a vector table entry, i.e. a function pointer.
 ///
 /// *Note: An empty entry (reserved) is the null pointer.*
-pub struct Vector(#[allow(unused)] *const ());
+#[repr(transparent)]
+pub struct Vector(Option<unsafe extern "C" fn()>);
 
 impl Vector {
     /// Create a vector with the provided function pointer.
     pub const fn handler(f: unsafe extern "C" fn()) -> Self {
-        Self(f as _)
+        Self(Some(f))
     }
 
     /// Create a vector that is reserved.
     pub const fn reserved() -> Self {
-        Self(core::ptr::null())
+        Self(None)
     }
 }
 
 /// # Safety
 ///
-/// This impl is needed due to the underlying
-/// `*const ()` value. This value is never
+/// This impl is needed due to the underlying *const ()` value. This value is never
 /// read, and as such is `Sync`.
 unsafe impl Sync for Vector {}
